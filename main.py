608744863621a877c5439f0f0923fb3dbcf65bd5
ZaidAG <72582743+ZaidAG@users.noreply.github.com>
2022-01-04 18:12:24 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask,jsonify,request
+import csv
+all_articles=[]
+with open('articles.csv')as f:
+    reader=csv.reader(f)
+    data=list(reader)
+    all_articles=data[1:]
+liked_articles=[]
+not_like_articles=[]
+did_not_read=[]
+app=Flask(__name__)
+@app.route('/get-article')
+def get_movie():
+    return jsonify({
+        "data":all_articles[0],
+        "status":"success"
+    })
+@app.route('/liked-article',methods=['POST'])
+def liked_article():
+    movie=all_articles[0]
+    all_article=all_articles[1:]
+    liked_articles.append(movie)
+    return jsonify({
+        "status":'success'
+    }),201
+@app.route('/unliked-article',methods=['POST'])
+def unliked_article():
+    article=all_articles[0]
+    all_article=all_articles[1:]
+    not_like_articles.append(article)
+    return jsonify({
+        "status":"success"
+    }),201
+@app.route('/did-not-read',methods=['POST'])
+def did_not_read():
+    article=all_articles[0]
+    all_article=all_articles[1:]
+    did_not_read.append(article)
+    return jsonify({
+        "status":"success"
+    }),201
+if __name__=="__main__":
+    app.run()
